917232a990f5cc8223c18158677b0b8288f827d3
lawrencecchen <54008264+lawrencecchen@users.noreply.github.com>
2026-05-30 19:33:22 -0700

    Build release app with macOS 26 SDK

--- Sources/TextBoxInput.swift
+++ Sources/TextBoxInput.swift
@@ -22,8 +22,8 @@ private enum TextBoxLayout {
     static let iconSymbolSize: CGFloat = 13
     static let sendSymbolSize: CGFloat = 14
     static let buttonBottomPadding: CGFloat = 3
-    static let leadingButtonHorizontalOffset: CGFloat = -1
-    static let trailingButtonHorizontalOffset: CGFloat = 1
+    static let leadingButtonHorizontalOffset: CGFloat = -2
+    static let trailingButtonHorizontalOffset: CGFloat = 2
     static let attachmentControlSpacing: CGFloat = 2
     static let attachmentImageSize: CGFloat = 16
     static let attachmentChipHeight: CGFloat = 18
